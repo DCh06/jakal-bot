53ff15723bea76d4940e7175be232becd38bc04e
Daniel Choleva <daniel.choleva@betsys.com>
2023-11-19 01:19:32 +0100

    im in sad

--- src/commands/standup/pripominam-standup.ts
+++ src/commands/standup/pripominam-standup.ts
@@ -3,11 +3,13 @@ import prisma from "../../db";
 import { client } from "../../implementation/client";
 import dayjs from 'dayjs'
 import * as utc from 'dayjs/plugin/utc'
+import * as timezone from 'dayjs/plugin/timezone'
 import { getDateSpanMilis } from "../../utils/random-generators";
 import { pripominumStandupJobGroCronuNehe } from "../../recurring-jobs/pripominam-standup-job";
 import { PripominamStandup } from "@prisma/client";
 
-dayjs.extend(utc.default)
+dayjs.extend(utc.default);
+dayjs.extend(timezone.default);
 
 
 // import prisma from '../../lib/prisma' 
@@ -37,21 +39,31 @@ export const command = {
         const dateString = (interaction.options.get('cas')?.value) as unknown as string;
         const dateOfStandup = new Date(dateString);
         const utcTimeOfStandup = dayjs.utc(dateString).toDate();
-        const utcTimeOfStandup2 = dayjs(dateString).toDate();
-        const utcTimeOfStandup3 = dayjs(dateString).utc().toDate();
-        
+        // const utcTimeOfStandup = dayjs.utc(dateString).locale();
+        // const utcTimeOfStandup2 = dayjs(dateString).locale();
+        // const utcTimeOfStandup3 = dayjs(dateString).utc().locale();
+        const utcTimeOfStandup2 = dayjs.tz(new Date(), "Europe/Prague") // '2013-11-18T11:55:20-05:00'
+        const utcTimeOfStandup3 = dayjs.tz(new Date(), "Europe/Prague").toDate() // '2013-11-18T11:55:20-05:00'
+        const utcTimeOfStandup4 = dayjs.tz(new Date(), "Europe/Prague").utc().toDate() // '2013-11-18T11:55:20-05:00'
+
+        // Converting (from time zone 'Europe/Berlin'!)
+        const utcTimeOfStandup5 =  dayjs(new Date()).tz("Europe/Prague")
+        const utcTimeOfStandup6 =  dayjs(new Date()).tz("Europe/Prague").toDate()
+        const utcTimeOfStandup7 =  dayjs(new Date()).tz("Europe/Prague").utc().toDate()
+
+
         const discordId = interaction.user.id;
         const description = interaction.options.get('popis') as unknown as string;
         const channelId = interaction.channelId;
-        console.log(utcTimeOfStandup,utcTimeOfStandup2, utcTimeOfStandup3);
-        
+        console.log(utcTimeOfStandup3, utcTimeOfStandup4, utcTimeOfStandup6, utcTimeOfStandup7);
+
         // if less than 30 dont save to db
-        if( getDateSpanMilis(new Date(), dateOfStandup) < 30 * 60 * 1000) {
+        if (getDateSpanMilis(new Date(), dateOfStandup) < 30 * 60 * 1000) {
             // TODO maybe this will fuckup 
 
-            let hackyPickData: Pick<PripominamStandup, 'time' | 'channelId'> = {time: dateOfStandup, channelId};
+            let hackyPickData: Pick<PripominamStandup, 'time' | 'channelId'> = { time: dateOfStandup, channelId };
             pripominumStandupJobGroCronuNehe([hackyPickData]);
-            interaction.editReply({content: "Dej si! "});
+            interaction.editReply({ content: "Dej si! " });
             return;
         }
 
@@ -64,12 +76,12 @@ export const command = {
                     channelId,
                 }
             });
-            console.log("in db",pripominamStandup);
-            
+            console.log("in db", pripominamStandup);
 
-            interaction.editReply({content: "Dej si! "})
-        } catch ( e ) {
-           
+
+            interaction.editReply({ content: "Dej si! " })
+        } catch (e) {
+
             console.log(e)
         }
         // todo create user if not exist
